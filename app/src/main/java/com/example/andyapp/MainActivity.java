8454c7b989ec2c4300caa2598233cce95ffd1587
Satxish <152068703+Satxish@users.noreply.github.com>
2025-04-16 14:11:43 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/andyapp/MainActivity.java
+++ app/src/main/java/com/example/andyapp/MainActivity.java
@@ -177,7 +177,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Remaining users for RecyclerView
         List<LeaderboardUser> remainingUsers = users.size() > 3
-                ? users.subList(4, users.size())
+                ? users.subList(3, users.size())
                 : new ArrayList<>();
 
         for (LeaderboardUser user : remainingUsers) {
